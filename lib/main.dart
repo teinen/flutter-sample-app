cedd48145bb86d135a0518fbb837f8e3aaa49e06
Akira Kanno <teinen.github@gmail.com>
2019-08-16 09:58:10 +0900

    Modify theme color

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,10 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'Startup Name Generator', home: RandomWords());
+    return MaterialApp(
+        title: 'Startup Name Generator',
+        theme: ThemeData(primaryColor: Colors.white),
+        home: RandomWords());
   }
 }
 
